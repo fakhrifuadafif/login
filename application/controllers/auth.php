722952c7fbf4c5abe4f91f86ba9c088f6747d4d7
Fakhri Fuad Afif <fakhrifuad57@gmail.com>
2023-03-09 12:00:16 +0700

    1054 error code

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -3,6 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
     public function index()
     {
         $data['title'] = 'User Login';
@@ -13,10 +18,34 @@ class auth extends CI_Controller
     
     public function regis()
     {
-        $data['title'] = 'User Registration';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/regis');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
 
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[5]|matches[password2]', ['
+            matches' => 'Password dont match',
+            'min_length' => 'password to short!'
+        ]);
+        
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if($this->form_validation->run() == false){
+            $data['title'] = 'User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/regis');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'),PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+            $this->db->insert('user',$data);
+            redirect('auth');
+        }
     }
 }
--- application/views/auth/regis.php
+++ application/views/auth/regis.php
@@ -11,17 +11,20 @@
                             </div>
                             <form class="user" method="post" action="<?base_url('auth/regis')?>">
                                 <div class="form-group">
-                                        <input type="text" class="form-control form-control-user" id="name" name="name"
-                                        placeholder="Full Name">
+                                    <input type="text" class="form-control form-control-user" id="name" name="name"
+                                    placeholder="Full Name" value="<?=set_value('name')?>">
+                                    <?= form_error('name',  '<small class="text-danger pl-3" >','</small>' ); ?>
                                 </div>
                                 <div class="form-group">
                                     <input type="text" class="form-control form-control-user" id="email" name="email"
-                                        placeholder="Email Address">
+                                        placeholder="Email Address"value="<?=set_value('email')?>">
+                                        <?= form_error('email',  '<small class="text-danger pl-3" >','</small>' ); ?>
                                 </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="password" class="form-control form-control-user"
                                             id="password1" name="password1" placeholder="Password">
+                                            <?= form_error('password1',  '<small class="text-danger pl-3" >','</small>' ); ?>
                                     </div>
                                     <div class="col-sm-6">
                                         <input type="password" class="form-control form-control-user"
